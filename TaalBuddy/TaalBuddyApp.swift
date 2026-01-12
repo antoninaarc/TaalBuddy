39ebfebf3f5662d61574ca021439fba156067703
antoninaarc <antoninarivera91@gmail.com>
2026-01-12 12:07:53 +0100

    Initial commit - TaalBuddy project setup

---
+++ TaalBuddy/TaalBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaalBuddyApp.swift
+//  TaalBuddy
+//
+//  Created by nini on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaalBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
